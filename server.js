1a38711e03154c7e5e9798fc7d5ca3121b21c41a
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-18 10:11:37 -0700

    Updated token size

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/generate', async (req, res) => {
                model: "gpt-3.5-turbo",
                messages: [{ role: "user", content: prompt }],
                temperature: 1,
-               max_tokens: 150,
+               max_tokens: 500,
                top_p: 1,
                frequency_penalty: 0,
                presence_penalty: 0,
